67ebbfdddf0c9e14e6fb2254900d145ef40fa06e
GlennOnyango <glennonyango@yahoo.com>
2022-08-26 07:46:35 +0300

    useReducer added to entire project

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -2,28 +2,36 @@ import React, { useEffect, useState } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
+import useHttp from '../custom_hooks/http';
 
 const Search = React.memo(props => {
   const {onFilter} =  props;  
   const [enteredfilter,setFilter] = useState("");
+  const {sendRequest, data, reqIdenifire} = useHttp(); 
+
 
   useEffect(()=>{
     const query = enteredfilter.length === 0 ? '' :
     `?orderBy="title"&equalTo="${enteredfilter}"`;
-    fetch("https://react-hooks-71b1e-default-rtdb.firebaseio.com/ingredients.json"+query)
-    .then(response => response.json())
-    .then(responseData => {
+    sendRequest("https://react-hooks-71b1e-default-rtdb.firebaseio.com/ingredients.json"+query,
+    "GET", null, null, "Req_Search"
+    );
+  },[sendRequest,enteredfilter]);
+
+  useEffect(() => {
+    if(reqIdenifire === "Req_Search"){
       const loadedIngredients = [];
-      for(const key in responseData){
+      for(const key in data){
           loadedIngredients.push({
             id:key,
-            title:responseData[key].title,
-            amount:responseData[key].amount
+            title:data[key].title,
+            amount:data[key].amount
           });
       }
       onFilter(loadedIngredients);
-    });
-  },[enteredfilter,onFilter]);
+    }
+  }, [reqIdenifire,data,onFilter]);
+  
 
   return (
     <section className="search">
